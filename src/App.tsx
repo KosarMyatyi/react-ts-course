1275cd3fb3fe2a23cadeba5863d00fd59f9519ad
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-04-14 19:26:08 +0300

    axios query optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './component/Card';
+import UserList from './component/UserList';
+import { IUser } from './types/types';
+import axios from 'axios';
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+      fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+      try {
+          const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+          setUsers(response.data)
+      } catch (e) {
+          alert(e)
+      }
+  }
+
   return (
     <div>
-      <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
+      <Card variant={CardVariant.outlined} width='200px' height='200px'>
         <button>Кнопка</button>
         <div>dfsdfsdf</div>
       </Card>
+      <UserList users={users} />
     </div>
   )
 }
--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
     width?: string;
     height?: string;
     variant: CardVariant;
-    onClick: (num: number) => void;
+    // onClick: (num: number) => void;
 }
 
 const Card: FC<PropsWithChildren<CardProps>> =
@@ -17,17 +17,14 @@ const Card: FC<PropsWithChildren<CardProps>> =
         width,
         height,
         variant,
-        onClick,
         children
     }) => {
-    const [state, setState] = useState(0)
+        const [state, setState] = useState(0)
         return (
             <div style={{
                 width, height, border: variant === CardVariant.outlined ? '1px solid black' : 'none',
                 background: variant === CardVariant.primary ? 'lightgrey' : ''
-            }}
-                onClick={() => onClick(state)}
-            >
+            }}>
                 {children}
             </div>
         )
