ea99f3b9de25ff0fabfd67be2b1f3ba78ec331f2
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-04-14 23:56:39 +0300

    add Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './component/Card';
-import UserList from './component/UserList';
-import { IUser } from './types/types';
+import { ITodo, IUser } from './types/types';
 import axios from 'axios';
+import List from './component/List';
+import UserItem from './component/UserItem';
+import TodoItem from './component/TodoItem';
 
 const App = () => {
   const [users, setUsers] = useState<IUser[]>([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   useEffect(() => {
-      fetchUsers()
+    fetchUsers()
+    fetchTodos()
   }, [])
 
   async function fetchUsers() {
-      try {
-          const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-          setUsers(response.data)
-      } catch (e) {
-          alert(e)
-      }
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      setTodos(response.data)
+    } catch (e) {
+      alert(e)
+    }
   }
 
   return (
@@ -26,7 +39,14 @@ const App = () => {
         <button>Кнопка</button>
         <div>dfsdfsdf</div>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   )
 }
